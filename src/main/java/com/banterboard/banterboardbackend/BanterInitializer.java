a682313945facc4b4e0a0d82f583cb558de8e608
Ryan Pelletier <pelletier_ryan@bah.com>
2020-10-06 22:56:15 -0400

    add save method

--- src/main/java/com/banterboard/banterboardbackend/BanterInitializer.java
+++ src/main/java/com/banterboard/banterboardbackend/BanterInitializer.java
@@ -24,19 +24,19 @@ public class BanterInitializer {
         banterRepository.deleteAll();
 
         Banter banter1 = new Banter();
-        banter1.setId(UUID.randomUUID().toString());
+        banter1.setId("1");
         banter1.setContext("Ryan was making pancakes.");
         banter1.setStory("Cole was like blah blah 'build pancakes'");
         banter1.setTime(Instant.now());
 
         Banter banter2 = new Banter();
-        banter2.setId(UUID.randomUUID().toString());
+        banter2.setId("2");
         banter2.setContext("Rachelle was being congratulated about something and Ryan joined in, not knowing what it was");
         banter2.setStory("El jokingly said 'Ryan her dog died', not knowing that it actually had");
         banter2.setTime(Instant.now().plusSeconds(3600));
 
         Banter banter3 = new Banter();
-        banter3.setId(UUID.randomUUID().toString());
+        banter3.setId("3");
         banter3.setContext("At El's house helping her move in.");
         banter3.setStory("Ryan made a joke that was really dumb.");
         banter3.setTime(Instant.now().plusSeconds(3600 * 2));
--- src/main/java/com/banterboard/banterboardbackend/api/controller/BanterController.java
+++ src/main/java/com/banterboard/banterboardbackend/api/controller/BanterController.java
@@ -5,6 +5,7 @@ import com.banterboard.banterboardbackend.repositories.BanterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +21,23 @@ public class BanterController {
     }
 
     /**
+     * Example Curl:
+     *
+      curl -X POST \
+        -H "Content-Type: application/json" \
+        --data '{"context":"Jason was talking about an ex.","story":"He said, first of all I totally deserve to have someone like this in my life"}' \
+        http://localhost:8080/banter-board/api/banter
+     *
+     * @param banter
+     */
+    @PostMapping(consumes = "application/json")
+    public void createBanter(@RequestBody Banter banter) {
+        banter.setId(UUID.randomUUID().toString());
+        banter.setTime(Instant.now());
+        banterRepository.save(banter);
+    }
+
+    /**
      *Example Curl:
      *
      * curl -X GET http://localhost:8080/banter-board/api/banter
@@ -31,23 +49,41 @@ public class BanterController {
         return banterRepository.findAll();
     }
 
+
+    /**
+     *Example Curl:
+     *
+     * curl -X GET http://localhost:8080/banter-board/api/banter/93df3cf9-6c5b-44f2-ba26-6257efd98b2
+     *
+     * @return
+     */
     @GetMapping(value = "/{id}", produces = "application/json")
     public Banter getBanter(@PathVariable String id){
         return banterRepository.findById(id).get();
     }
 
-    @PostMapping
-    public void createBanter(@RequestBody Banter banter) {
-        banter.setId(UUID.randomUUID().toString());
-        banterRepository.save(banter);
-    }
 
+    /**
+     *Example Curl:
+     *
+     * curl -X PUT http://localhost:8080/banter-board/api/banter/93df3cf9-6c5b-44f2-ba26-6257efd98b2
+     *
+     * @return
+     */
     @PutMapping("/{id}")
     public void updateBanter(@RequestBody Banter banter){
         //TODO not sure how this should work
         banterRepository.save(banter);
     }
 
+
+    /**
+     *Example Curl:
+     *
+     * curl -X DELETE http://localhost:8080/banter-board/api/banter/93df3cf9-6c5b-44f2-ba26-6257efd98b2
+     *
+     * @return
+     */
     @DeleteMapping("/{id}")
     public void deleteBanter(@PathVariable String id){
         banterRepository.deleteById(id);
